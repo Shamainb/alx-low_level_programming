af8248c98ca7267717b802cf9d6a45c01dc7fece
Shamainb <shamainbhengesa60@gmail.com>
2023-02-22 15:41:36 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print alphabet - Make the alphabet
+ * Return: void
+ */
+
+void (print_alphabet(void))
+{
+	char c;
+        
+	for(c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(0);
+	}
+	_putchar('\n');
+}
