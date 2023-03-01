9b0b7e0d6e20b7e1434d44eb9fb4f97b2d03cf17
Shamainb <shamainbhengesa60@gmail.com>
2023-03-01 07:07:56 -0800

    task more functions nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,22 +2,22 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of time the character _ should be printed
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
 		int i;
 
-		for (i ==1; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
-}	
+
+}
