c3969481c607fb1ea10e0a73a64f0ba1612e1dc7
Shamainb <shamainbhengesa60@gmail.com>
2023-07-18 17:01:12 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - void
+ *  a function that prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int alp;
+
+	while (i < 10)
+	i++;
+		{
+		for (alp = 97; alp <= 122; alp++)
+		putchar (alp);
+		}
+		putchar('\n');
+}
