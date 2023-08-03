693fecd4abb77c4186f5ad1afcc0350848810c57
Shamainb <shamainbhengesa60@gmail.com>
2023-08-03 03:16:07 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ * @s: string to be printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		printf("\n");
+	return;
+
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+}
