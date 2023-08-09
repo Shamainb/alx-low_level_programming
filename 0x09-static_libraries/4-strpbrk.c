45985a351bca16925456d496f6c4220c63622be8
Shamainb <shamainbhengesa60@gmail.com>
2023-08-09 16:07:33 -0700

    task 0

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s: input
+ * @accept: input
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        char *n = accept;
+
+        while (*s)
+{
+        while (*n)
+        {
+                if (*s == *n)
+                {
+                        return (s);
+                }
+                n++;
+        }
+        s++;
+}
+return (NULL);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+        char *i = haystack;
+        char *s = needle;
+
+        for (; *haystack != '\0'; haystack++)
+        {
+        while (*i == *s && *s != '\0')
+        {
+                i++;
+                s++;
+        }
+        if (*s == '\0')
+                return (haystack);
+        }
+return (0);
+}
