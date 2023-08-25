57bf4f92f4a328e955f98e3720c7287c69d0d79b
Shamainb <shamainbhengesa60@gmail.com>
2023-08-24 19:16:32 -0700

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+	}
+}
