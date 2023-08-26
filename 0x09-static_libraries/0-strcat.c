e3c872c1cd2d95a77533b71850f75f6f0281ab82
Shamainb <shamainbhengesa60@gmail.com>
2023-08-26 05:37:56 -0700

    tasklibraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,32 @@
-char *_strcat(char *dest, char *src);
+#include "main.h"
+
+/**
+ * _strcat - function concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int t;
+
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	t = 0;
+	while (src[t] != '\0')
+{
+		dest[i] = src[t];
+		i++;
+		t++;
+	}
+
+
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,1 +1,29 @@
-char *_strstr(char *haystack, char *needle);
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *i = haystack;
+	char *n = needle;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		while (*i == *n && *n != '\0')
+		{
+			i++;
+			n++;
+		}
+
+
+		if (*n == '\0')
+			return (haystack);
+	}
+
+
+	return (0);
+}
