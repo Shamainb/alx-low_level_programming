e79826ad28ccd86511644d2c1390ab4e373fe881
Shamainb <shamainbhengesa60@gmail.com>
2023-08-29 22:09:47 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,21 +11,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *r;
-	unsigned int len_s1 = strlen(s1);
-	unsigned int len_s2 = strlen(s2);
-	unsigned int total_len;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (n >= len_s2)
-	{
-		total_len = len_s1 + n + 1;
-	r = (char *)malloc(total_len);
-	if (r == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
 	}
-	strcpy(r, s1);
 
-	strncat(r, s2, n);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	return (r);
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
+
 }
