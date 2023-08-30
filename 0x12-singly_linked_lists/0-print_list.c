2a16b5b3694211dfeee68232e1e388ec52d16012
Shamainb <shamainbhengesa60@gmail.com>
2023-08-30 09:12:23 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t cnt = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -18,7 +18,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-			cnt++;
+		s++;
 	}
-		return (cnt);
+
+	return (s);
 }
