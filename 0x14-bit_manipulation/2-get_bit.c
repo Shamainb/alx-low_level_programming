fc277fb7da8a1549499a1b3ac97a02189e3f4bf9
Shamainb <shamainbhengesa60@gmail.com>
2023-09-22 02:11:10 -0700

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 /**
  * get_bit - a function that returns the value of a bit
  * @n: number to check
- * #index:  is the index, starting from 0
+ * @index: is the index, starting from 0
  * Return: the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
@@ -12,6 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > 63)
 		return (-1);
+
 	bit = (n >> index) & 1;
 
 	return (bit);
