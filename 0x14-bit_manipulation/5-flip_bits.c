eafd0e649464354a5160ab0889aaf84b609916e2
Shamainb <shamainbhengesa60@gmail.com>
2023-09-22 05:41:35 -0700

    task5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits -  function that returns the number of bits
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i;
+	int cnt = 0;
+	unsigned long int c;
+	unsigned long int ex = n ^ m;
+
+	for (i = 63; i >= 0; i++)
+	{
+		c = ex >> i;
+		if (c & 1)
+			c++;
+	}
+	return (cnt);
+}
